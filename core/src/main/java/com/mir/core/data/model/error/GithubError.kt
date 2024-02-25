87c535f3348637f5c9015d0f2bd3c1cef07dae46
Anika1d <t1kend.offical@gmail.com>
2024-02-25 19:33:17 +1000

    project:core:model:error:GithubError.kt fix

--- core/src/main/java/com/mir/core/data/model/error/GithubError.kt
+++ core/src/main/java/com/mir/core/data/model/error/GithubError.kt
@@ -1,6 +1,9 @@
 package com.mir.core.data.model.error
 
 import com.mir.core.data.response.error.Error
+import kotlinx.serialization.Serializable
 
-class GithubError(errors: List<Error>, message: String) {
+
+@Serializable
+class GithubError(val errors: List<Error>, val message: String) {
 }
