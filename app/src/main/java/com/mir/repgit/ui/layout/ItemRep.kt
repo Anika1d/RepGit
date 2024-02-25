86c237458abe055bdf40dfe0782a5ba529940dae
Anika1d <t1kend.offical@gmail.com>
2024-02-25 15:47:52 +1000

    project:layout:ItemRep:update design for repository model

--- app/src/main/java/com/mir/repgit/ui/layout/ItemRep.kt
+++ app/src/main/java/com/mir/repgit/ui/layout/ItemRep.kt
@@ -1,6 +1,5 @@
 package com.mir.repgit.ui.layout
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -10,23 +9,33 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.mir.core.data.model.repository.Owner
+import com.mir.core.data.model.repository.RepositoryItem
 import com.mir.repgit.R
+import com.mir.repgit.tools.SizeManager
 
 @Composable
 fun ItemRep(
     modifier: Modifier,
     contentPadding: PaddingValues = PaddingValues(8.dp),
+    repository: RepositoryItem,
     onClick: () -> Unit,
 ) {
     Box(modifier = modifier
@@ -53,13 +62,24 @@ fun ItemRep(
             )
         }) {
         Row(Modifier.padding(paddingValues = contentPadding)) {
-            Image(
-                painter = painterResource(id = R.drawable.avatar_default),
-                contentDescription = "avatar default"
+            AsyncImage(
+                model = repository.owner.avatarUrl.ifEmpty { painterResource(id = R.drawable.avatar_default) },
+                contentDescription = repository.owner.avatarUrl.ifEmpty { "avatar_owner" },
+                placeholder = painterResource(id = R.drawable.avatar_default),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(
+                        SizeManager(
+                            LocalConfiguration.current.screenHeightDp,
+                            LocalConfiguration.current.screenWidthDp
+                        ).giveNeed(40.dp, 60.dp, 80.dp)
+                    )
+                    .clip(CircleShape)
             )
+
             Column {
-                Text(text = " name", color = Color.White)
-                Text(text = " Description", color = Color.White)
+                Text(text = repository.name, color = Color.White)
+                Text(text = repository.description ?: "", color = Color.White)
             }
 
         }
@@ -75,6 +95,11 @@ fun PreviewItemRep() {
             .fillMaxWidth()
             .height(120.dp),
         contentPadding = PaddingValues(8.dp),
+        repository = RepositoryItem(
+            owner = Owner(""),
+            description = "Desc",
+            name = "Repka"
+        ),
     ) {
 
     }
