3f51f96542b733b72b880cd04ecbc2ee3c42be56
Anika1d <t1kend.offical@gmail.com>
2024-02-26 20:03:16 +1000

    project:core:repository:add Repository and Issues request

--- core/src/main/java/com/mir/core/repository/IGithubRepository.kt
+++ core/src/main/java/com/mir/core/repository/IGithubRepository.kt
@@ -1,10 +1,17 @@
 package com.mir.core.repository
 
+import com.mir.core.data.model.issues.Issues
 import com.mir.core.data.model.repository.PageRepository
+import com.mir.core.data.model.repository.RepositoryItem
+import com.mir.core.data.request.IssuesRequest
+import com.mir.core.data.request.RepositoryRequest
 import com.mir.core.data.request.SearchRequest
 import com.mir.core.data.response.ResultState
 import kotlinx.coroutines.flow.Flow
 
 interface IGithubRepository{
     suspend fun search(searchRequest: SearchRequest): Flow<ResultState<PageRepository>>
+    suspend fun getRepository(repositoryRequest: RepositoryRequest): Flow<ResultState<RepositoryItem>>
+
+    suspend fun getIssues(issuesRequest:IssuesRequest): Flow<ResultState<List<Issues>>>
 }
--- core/src/main/java/com/mir/core/repository/ImplGithubRepository.kt
+++ core/src/main/java/com/mir/core/repository/ImplGithubRepository.kt
@@ -1,11 +1,17 @@
 package com.mir.core.repository
 
 import android.util.Log
+import com.mir.core.data.model.issues.Issues
 import com.mir.core.data.model.repository.PageRepository
+import com.mir.core.data.model.repository.RepositoryItem
+import com.mir.core.data.request.IssuesRequest
+import com.mir.core.data.request.RepositoryRequest
 import com.mir.core.data.request.SearchRequest
 import com.mir.core.data.response.ResultState
 import com.mir.core.data.response.error.ErrorResponse
+import com.mir.core.data.response.issues.IssuesItemResponse
 import com.mir.core.data.response.pagerep.PageResponse
+import com.mir.core.data.response.repository.RepositoryResponse
 import com.mir.core.mapper.Mapper
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -43,7 +49,7 @@ class ImplGithubRepository : IGithubRepository, KoinComponent {
                     val rep = s.body<PageResponse>()
                     emit(
                         ResultState.Success(
-                            Mapper.mapRepositoryResponseToAppModel(
+                            Mapper.mapPageRepositoryResponseToAppModel(
                                 pageResponse = rep,
                                 nextPage = nextPage?.substring(
                                     nextPage.indexOf("<") + 1,
@@ -52,6 +58,67 @@ class ImplGithubRepository : IGithubRepository, KoinComponent {
 
                             )
                         )
+                    )
+                } else
+                    emit(
+                        ResultState.NetworkError(
+                            Mapper.mapErrorResponseToAppModel(s.body<ErrorResponse>()),
+                            s.status
+                        )
+                    )
+
+            } catch (ce: CancellationException) {
+                Log.e("infos", ce.stackTrace.toString())
+                emit(ResultState.Error(ce, HttpStatusCode.TooManyRequests))
+                throw ce
+            } catch (e: Exception) {
+                emit(ResultState.Error(e, HttpStatusCode.BadRequest))
+            }
+        }.flowOn(Dispatchers.IO)
+    }
+
+    override suspend fun getRepository(repositoryRequest: RepositoryRequest): Flow<ResultState<RepositoryItem>> {
+        return flow {
+            try {
+                val s = client.get("/repos/${repositoryRequest.nameOwner}/${repositoryRequest.nameRepository}")
+                Log.println(Log.INFO, "infos", s.toString())
+                if (s.status == HttpStatusCode.OK) {
+
+                    val rep = s.body<RepositoryResponse>()
+                    emit(
+                        ResultState.Success(
+                            Mapper.mapRepositoryResponseToAppModel(rep)
+                        )
+                    )
+                } else
+                    emit(
+                        ResultState.NetworkError(
+                            Mapper.mapErrorResponseToAppModel(s.body<ErrorResponse>()),
+                            s.status
+                        )
+                    )
+
+            } catch (ce: CancellationException) {
+                Log.e("infos", ce.stackTrace.toString())
+                emit(ResultState.Error(ce, HttpStatusCode.TooManyRequests))
+                throw ce
+            } catch (e: Exception) {
+                emit(ResultState.Error(e, HttpStatusCode.BadRequest))
+            }
+        }.flowOn(Dispatchers.IO)
+    }
+
+    override suspend fun getIssues(issuesRequest: IssuesRequest): Flow<ResultState<List<Issues>>> {
+        return flow {
+            try {
+                val s = client.get("/repos/${issuesRequest.nameOwner}/${issuesRequest.nameRepository}/issues")
+                Log.println(Log.INFO, "infos", s.toString())
+                if (s.status == HttpStatusCode.OK) {
+                    val i=s.body<String>()
+                    val issues = s.body<List<IssuesItemResponse>>()
+                    println(i)
+                    emit(
+                        ResultState.Success(Mapper.mapIssuesResponseToAppModel(issues))
                     )
                 } else
                     emit(
