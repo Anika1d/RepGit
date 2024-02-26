57e27ca7f5508999c36f11da4cf1253104593c3a
Anika1d <t1kend.offical@gmail.com>
2024-02-26 20:07:06 +1000

    project:app:ui:theme:add colors

--- app/src/main/java/com/mir/repgit/ui/theme/Color.kt
+++ app/src/main/java/com/mir/repgit/ui/theme/Color.kt
@@ -9,3 +9,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val darkBlue = Color(0xFF010A43)
+val mint = Color(0xFF1DC7AC)
+val blue=Color(0xFF10194E)
+
+val defaultColors = listOf(
+    darkBlue,
+    mint,
+    blue,
+)
