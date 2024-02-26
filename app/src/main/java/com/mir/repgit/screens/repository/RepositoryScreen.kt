0746bda3007644b2fd04c30061bc5a9eb9a2bad7
Anika1d <t1kend.offical@gmail.com>
2024-02-26 21:43:05 +1000

    project:app:screens:RepositoryScreen:add pullToRefreshContainer and repository details

--- app/src/main/java/com/mir/repgit/screens/repository/RepositoryScreen.kt
+++ app/src/main/java/com/mir/repgit/screens/repository/RepositoryScreen.kt
@@ -1,7 +1,7 @@
 package com.mir.repgit.screens.repository
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -12,6 +12,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.BottomSheetScaffold
@@ -19,32 +22,92 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.mir.repgit.R
+import com.mir.repgit.tools.SizeManager
+import com.mir.repgit.tools.composable.placeholder
 import com.mir.repgit.ui.layout.BackgroundContainer
+import com.mir.repgit.ui.layout.ItemIssue
+import com.mir.repgit.ui.layout.RepositoryDetails
+import com.mir.repgit.ui.theme.blue
+import com.mir.repgit.ui.theme.dirtyWhite
+import com.mir.repgit.ui.theme.mint
 import com.mir.repgit.values.LocalNavController
-
+import com.mir.repgit.viewmodel.RepositoryViewModel
+import kotlinx.coroutines.launch
+import org.koin.compose.koinInject
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun RepositoryScreen(idRep: Long?) {
+fun RepositoryScreen(owner: String, repo: String) {
     val sheetState = rememberBottomSheetScaffoldState()
     val coroutineScope = rememberCoroutineScope()
+    val viewModel = koinInject<RepositoryViewModel>()
     val navController = LocalNavController.current!!
-    BackgroundContainer(modifier = Modifier.fillMaxSize()) {
+    val repository = viewModel.repository.observeAsState().value
+    val issues = viewModel.issues.observeAsState().value
+    val repLog = viewModel.dataRepositoryReceived.observeAsState().value!!
+    remember(owner) {
+        coroutineScope.launch {
+            viewModel.clearData()
+            viewModel.getRepository(nameOwner = owner, nameRep = repo, onSuccess = {}, onError = {})
+            viewModel.getIssues(nameOwner = owner, nameRep = repo, onSuccess = {}, onError = {})
+        }
+    }
+
+    val pullToRefreshState = rememberPullToRefreshState()
+
+    val scaleFraction =
+        if (pullToRefreshState.isRefreshing) 1f else LinearOutSlowInEasing.transform(
+            pullToRefreshState.progress
+        ).coerceIn(0f, 1f)
+    if (pullToRefreshState.isRefreshing) LaunchedEffect(key1 = true) {
+        viewModel.getRepository(nameOwner = owner, nameRep = repo, onSuccess = {}, onError = {})
+        viewModel.getIssues(nameOwner = owner, nameRep = repo, onSuccess = {}, onError = {})
+        pullToRefreshState.endRefresh()
+    }
+    BackgroundContainer(
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(pullToRefreshState.nestedScrollConnection)
+    ) {
+        PullToRefreshContainer(modifier = Modifier
+            .align(Alignment.TopCenter)
+            .graphicsLayer(scaleX = scaleFraction, scaleY = scaleFraction),
+            contentColor = Color.Transparent,
+            containerColor = Color.Transparent,
+            state = pullToRefreshState,
+            indicator = { pullRefreshState ->
+                PullToRefreshDefaults.Indicator(
+                    state = pullRefreshState, color = mint
+                )
+            })
         Column(Modifier.fillMaxSize()) {
             Row(
                 modifier = Modifier
@@ -53,7 +116,7 @@ fun RepositoryScreen(idRep: Long?) {
                         drawLine(
                             start = Offset(0f, size.height),
                             end = Offset(size.width, size.height),
-                            color = Color(0xFF1DC7AC),
+                            color = mint,
                             alpha = 1f,
                             strokeWidth = 2.dp.toPx()
                         )
@@ -61,18 +124,22 @@ fun RepositoryScreen(idRep: Long?) {
                 horizontalArrangement = Arrangement.Start,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                IconButton(modifier = Modifier, onClick = { navController.navigateUp() }) {
+                IconButton(modifier = Modifier, onClick = {
+                    navController.navigateUp()
+                }) {
                     Icon(
                         Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription = "",
-                        tint = Color(0xFFFAFAFA)
+                        contentDescription = "arrow_back",
+                        tint = dirtyWhite
                     )
                 }
                 Text(
-                    modifier = Modifier.fillMaxWidth(0.8f),
-                    text = "Repository",
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .placeholder(enabled = !repLog),
+                    text = repository?.name ?: "",
                     textAlign = TextAlign.Center,
-                    color = Color.White
+                    color = dirtyWhite
                 )
             }
 
@@ -80,49 +147,140 @@ fun RepositoryScreen(idRep: Long?) {
                 scaffoldState = sheetState,
                 sheetContent = {
                     LazyColumn(
+                        modifier = Modifier.animateContentSize(),
                         content = {
-                            items(30) {
-                                Spacer(modifier = Modifier.size(20.dp))
+                            item {
                                 Text(
-                                    text = "text", modifier = Modifier
-                                        .fillMaxWidth()
-                                        .background(
-                                            Color(0x337B22A5)
-                                        )
-                                        .padding(4.dp),
-                                    style = TextStyle(fontSize = 20.sp)
+                                    text = "Issues", style = TextStyle(
+                                        fontSize = 50.sp
+                                    )
                                 )
+                            }
+                            items(issues?.size ?: 0) { index ->
+                                Spacer(modifier = Modifier.size(20.dp))
+                                ItemIssue(
+                                    modifier = Modifier.fillMaxWidth(), issue = issues!![index]
+                                )
+
                             }
                         },
                         contentPadding = PaddingValues(8.dp),
                     )
                 },
-                sheetContainerColor = Color(0xE610194E),
-                sheetContentColor = Color(0xFF1DC7AC),
+                sheetContainerColor = blue.copy(0.9f),
+                sheetContentColor = mint,
                 contentColor = Color.Transparent,
                 containerColor = Color.Transparent,
-
-
-                ) {
-                Row(
-                    Modifier.padding(
-                        paddingValues = PaddingValues(
-                            horizontal = 8.dp,
-                            vertical = 16.dp
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(
+                            paddingValues = PaddingValues(
+                                horizontal = 8.dp, vertical = 16.dp
+                            )
                         )
-                    )
+                        .verticalScroll(
+                            state = rememberScrollState()
+                        )
                 ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.avatar_default),
-                        contentDescription = "avatar default"
-                    )
-                    Column {
-                        Text(text = " name", color = Color.White)
-                        Text(text = " Description", color = Color.White)
-                    }
-                }
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        AsyncImage(
+                            model = repository?.owner?.avatarUrl,
+                            contentDescription = repository?.owner?.avatarUrl.toString(),
+                            placeholder = painterResource(id = R.drawable.avatar_default),
+                            error = if(repLog)painterResource(id = R.drawable.image_fail)else null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .size(
+                                    SizeManager(
+                                        LocalConfiguration.current.screenHeightDp,
+                                        LocalConfiguration.current.screenWidthDp
+                                    ).giveNeed(130.dp, 150.dp, 160.dp)
+                                )
+                                .clip(CircleShape)
+                                .placeholder(enabled = !repLog)
 
+                        )
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.SpaceEvenly
+                        ) {
+                            Text(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .placeholder(enabled = !repLog),
+                                text = repository?.owner?.name ?: "",
+                                color = dirtyWhite,
+                                style = TextStyle(
+                                    fontFamily = FontFamily.Serif, fontSize = 23.sp
+                                )
+                            )
+                            Spacer(modifier = Modifier.size(20.dp))
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+
+                                RepositoryDetails(
+                                    modifier = Modifier.fillMaxWidth(0.5f),
+                                    item = repository?.starCount,
+                                    painter = painterResource(id = R.drawable.star),
+                                    contentDescription = "git_star",
+                                    placeholder = !repLog
+                                )
+                                RepositoryDetails(
+                                    modifier = Modifier,
+                                    item = repository?.forkCount,
+                                    painter = painterResource(id = R.drawable.forks),
+                                    contentDescription = "git_forks",
+                                    placeholder = !repLog
+                                )
+                            }
+                            Spacer(modifier = Modifier.size(20.dp))
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                RepositoryDetails(
+                                    modifier = Modifier.fillMaxWidth(0.5f),
+                                    item = repository?.watchersCount,
+                                    painter = painterResource(id = R.drawable.watch),
+                                    contentDescription = "git_watching",
+                                    placeholder = !repLog
+                                )
+                                RepositoryDetails(
+                                    modifier = Modifier,
+                                    item = repository?.issuesCount,
+                                    painter = painterResource(id = R.drawable.issues),
+                                    contentDescription = "git_branches",
+                                    placeholder = !repLog
+                                )
+
+                            }
+
+
+                        }
+                    }
+
+                    Text(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(10.dp)
+                            .placeholder(enabled = !repLog),
+                        text = repository?.description ?: "",
+                        color = dirtyWhite,
+                    )
+                    Spacer(modifier = Modifier.size(100.dp))
+
+                }
             }
+
         }
     }
 }
@@ -131,5 +289,5 @@ fun RepositoryScreen(idRep: Long?) {
 @Preview(showBackground = true)
 @Composable
 fun PreviewRepositoryScreen() {
-    RepositoryScreen(1)
+    RepositoryScreen("Anika1d", "GitRep")
 }
