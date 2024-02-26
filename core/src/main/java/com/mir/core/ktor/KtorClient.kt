4f719944bd3750746f8e2cd57c57343c12a15b7b
Anika1d <t1kend.offical@gmail.com>
2024-02-26 20:01:57 +1000

    project:core:ktor:update HttpClient

--- core/src/main/java/com/mir/core/ktor/KtorClient.kt
+++ core/src/main/java/com/mir/core/ktor/KtorClient.kt
@@ -5,37 +5,45 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
-import io.ktor.http.ContentType
-import io.ktor.serialization.kotlinx.json.DefaultJson
-import io.ktor.serialization.kotlinx.json.json
 import io.ktor.client.plugins.logging.ANDROID
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.headers
+import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.URLProtocol
 import io.ktor.http.takeFrom
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
 object KtorClient {
     fun provide() = HttpClient(CIO) {
         this.install(ContentNegotiation) {
             json(
-                json = DefaultJson,
+                json = Json {
+                    ignoreUnknownKeys = true
+                    encodeDefaults = true
+                    isLenient = true
+                    allowSpecialFloatingPointValues = true
+                    prettyPrint = false
+                    useArrayPolymorphism = false
+                },
                 contentType = ContentType.Application.Json,
-                )
+            )
+
         }
         this.install(Logging) {
             logger = Logger.ANDROID
             level = LogLevel.ALL
         }
         defaultRequest {
-            url{
+            url {
                 takeFrom(BuildConfig.BASE_URL)
-                host=BuildConfig.HOST_NAME
-                protocol= URLProtocol.HTTPS
+                host = BuildConfig.HOST_NAME
+                protocol = URLProtocol.HTTPS
                 headers {
-                    append(HttpHeaders.Accept,"application/vnd.github+json")
+                    append(HttpHeaders.Accept, "application/vnd.github+json")
                     append(HttpHeaders.Authorization, "Bearer ${BuildConfig.GITHUB_AUTH_TOKEN}")
                 }
             }
