d91df3f68d4feb8937d0b03e9492b2453ad39373
Anika1d <t1kend.offical@gmail.com>
2024-02-26 20:36:16 +1000

    project:app:ui:theme:Color:add lineBackgroundColor(0xFFC4C4C4)

--- app/src/main/java/com/mir/repgit/ui/theme/Color.kt
+++ app/src/main/java/com/mir/repgit/ui/theme/Color.kt
@@ -10,6 +10,9 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+
+val lineBackgroundColor=Color(0xFFC4C4C4)
+
 val darkBlue = Color(0xFF010A43)
 val mint = Color(0xFF1DC7AC)
 val blue=Color(0xFF10194E)
