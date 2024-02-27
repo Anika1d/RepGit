e0bf23cae366a90da4c0ebec7210ca400cca0abb
Anika1d <t1kend.offical@gmail.com>
2024-02-27 22:46:25 +1000

    project:app:App:connect  koin module usecase database

--- app/src/main/java/com/mir/repgit/App.kt
+++ app/src/main/java/com/mir/repgit/App.kt
@@ -2,17 +2,19 @@ package com.mir.repgit
 
 import android.app.Application
 import org.koin.core.context.startKoin
-import com.mir.core.koin.coreModule
-import com.mir.core.koin.useCasesModule
+import com.mir.core.koin.useCasesCoreModule
+import com.mir.database.koin.useCaseDatabaseModule
 import com.mir.repgit.koin.viewModelModel
+import org.koin.android.ext.koin.androidContext
 
 class App : Application() {
     init {
         startKoin {
+            androidContext(this@App)
             modules(
-                coreModule,
                 viewModelModel,
-                useCasesModule,
+                useCasesCoreModule,
+                useCaseDatabaseModule,
             )
         }
     }
