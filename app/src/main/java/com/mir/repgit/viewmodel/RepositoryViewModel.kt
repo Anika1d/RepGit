5adb296ffc693ae1ae4dac312255e987ecbf2e8a
Anika1d <t1kend.offical@gmail.com>
2024-02-28 16:47:34 +1000

    project:app:viewmodel:RepositoryViewMode:merge get function

--- app/src/main/java/com/mir/repgit/viewmodel/RepositoryViewModel.kt
+++ app/src/main/java/com/mir/repgit/viewmodel/RepositoryViewModel.kt
@@ -1,8 +1,6 @@
 package com.mir.repgit.viewmodel
 
 import android.util.Log
-import dev.icerock.moko.mvvm.livedata.LiveData
-import dev.icerock.moko.mvvm.livedata.MutableLiveData
 import com.mir.core.data.model.issues.Issues
 import com.mir.core.data.model.repository.RepositoryItem
 import com.mir.core.data.request.IssuesRequest
@@ -12,6 +10,8 @@ import com.mir.core.usecase.IssuesUseCase
 import com.mir.core.usecase.RepositoryUseCase
 import com.mir.repgit.tools.network.ConnectivityState
 import com.mir.repgit.tools.network.ImplEthernetConnectivityService
+import dev.icerock.moko.mvvm.livedata.LiveData
+import dev.icerock.moko.mvvm.livedata.MutableLiveData
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -47,37 +47,38 @@ class RepositoryViewModel(
     val dataIssuesReceived: LiveData<Boolean>
         get() = _dataIssuesReceived
 
-     fun getRepository(
+  private  fun getRepository(
         nameRep: String, nameOwner: String,
         onSuccess: () -> Unit, onError: (String?) -> Unit
     ) {
         viewModelScope.launch {
-        repositoryUseCase.getRepository(
-            RepositoryRequest(
-                nameOwner = nameOwner,
-                nameRepository = nameRep
-            )
-        ).collect { result ->
-            when (result) {
-                is ResultState.Success -> {
-                    _repository.value=result.data
-                    onSuccess.invoke()
-                }
+            repositoryUseCase.getRepository(
+                RepositoryRequest(
+                    nameOwner = nameOwner,
+                    nameRepository = nameRep
+                )
+            ).collect { result ->
+                when (result) {
+                    is ResultState.Success -> {
+                        _repository.value = result.data
+                        onSuccess.invoke()
+                    }
 
-                is ResultState.NetworkError -> {
-                    onError.invoke(result.error.message)
-                }
+                    is ResultState.NetworkError -> {
+                        onError.invoke(result.error.message)
+                    }
 
-                is ResultState.Error -> {
-                    onError.invoke(result.error.message)
+                    is ResultState.Error -> {
+                        onError.invoke(result.error.message)
+                    }
                 }
+                _dataRepositoryReceived.value = true
             }
-            _dataRepositoryReceived.value=true
+
         }
+    }
 
-    }}
-
-     fun getIssues(
+   private fun getIssues(
         nameRep: String,
         nameOwner: String,
         onSuccess: () -> Unit,
@@ -92,7 +93,7 @@ class RepositoryViewModel(
             ).collect { result ->
                 when (result) {
                     is ResultState.Success -> {
-                        _issues.value=result.data
+                        _issues.value = result.data
                         Log.i("issuie", "success")
                         onSuccess.invoke()
                     }
@@ -114,9 +115,33 @@ class RepositoryViewModel(
     }
 
     fun clearData() {
-        _repository.value=null
-        _dataRepositoryReceived.value=false
-        _dataIssuesReceived.value=false
+        _repository.value = null
+        _dataRepositoryReceived.value = false
+        _dataIssuesReceived.value = false
 
+    }
+
+    fun getAllInfoRep(
+        owner: String, repo: String,
+        onError: (String?) -> Unit,
+        onSuccess: () -> Unit
+    ) {
+        var repError = ""
+        var issError = ""
+        getRepository(nameOwner = owner,
+            nameRep = repo,
+            onSuccess = {},
+            onError = { repError = it.toString() })
+        getIssues(nameOwner = owner,
+            nameRep = repo,
+            onSuccess = { },
+            onError = { issError = it.toString() })
+        if (_dataIssuesReceived.value && _dataRepositoryReceived.value) {
+            onSuccess.invoke()
+        } else {
+            if (repError == issError) onError.invoke(issError)
+            else onError.invoke("$repError $issError")
+
+        }
     }
 }
