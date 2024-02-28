81346f77b7586fe0eccbc7b63c345333410d0935
Anika1d <t1kend.offical@gmail.com>
2024-02-28 11:06:46 +1000

    project:app:koin:create module networkConnectivityModule

--- app/src/main/java/com/mir/repgit/koin/modules.kt
+++ app/src/main/java/com/mir/repgit/koin/modules.kt
@@ -1,22 +1,30 @@
 package com.mir.repgit.koin
 
+import com.mir.repgit.tools.network.ImplEthernetConnectivityService
 import com.mir.repgit.viewmodel.MainViewModel
 import com.mir.repgit.viewmodel.RepositoryViewModel
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
+internal val networkConnectivityModule = module {
+    single { ImplEthernetConnectivityService(context = androidContext()) }
+}
 val viewModelModel = module {
+    includes(networkConnectivityModule)
     single {
         MainViewModel(
             searchUseCase = get(),
             insertSearchQueryUseCase = get(),
             deleteSearchQueryUseCase = get(),
-            getSearchQueryUseCase = get()
+            getSearchQueryUseCase = get(),
+            implEthernetConnectivityService = get()
         )
     }
     single {
         RepositoryViewModel(
             issuesUseCase = get(),
-            repositoryUseCase = get()
+            repositoryUseCase = get(),
+            implEthernetConnectivityService = get()
         )
     }
 }
